83eba03663be147dba867fc6de582cb570fb403d
EDUARDO Jorge Andrade Mourao Oliveira B901599 <b901599@bnb.gov.br>
2024-07-12 12:04:12 -0300

    creating main.py file

---
+++ api/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+import httpx
+from bs4 import BeautifulSoup
+
+from services import openai
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8501",
+    "ohttps://8501-giuferreira-cursotecnic-cew0gqba5so.ws-us92.gitpod.i",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+async def getContent(url):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+    
+    soup = BeautifulSoup(response.content, 'html.parser')
+    content = soup.get_text()
+    return content
+
+@app.get("/", response_class = PlainTextResponse)
+async def root():
+    return "API GPT is Alive"
+
+@app.get("/summary", response_class = PlainTextResponse)
+async def summary(url: str):
+    content = await getContent(url)
+    summary = openai.getSummary(content)
+    return summary
+
+@app.get("/answer", response_class = PlainTextResponse)
+async def answer(url: str, question: str):
+    content = await getContent(url)
+    answer = openai.getAnswer(content, question)
+    return answer
